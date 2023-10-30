1d19e7c471434471ca206eac58135b933836ca58
kimdongjooon <ehdwns6745@naver.com>
2023-10-30 16:25:13 +0900

    react.js #2

--- 1.basics/ex05/src/App.js
+++ 1.basics/ex05/src/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
+
 function App() {
-    const App = document.createElement('div');
-    App.textContent = "basic.ex04 - Hello Wolrd";
-    return App;
+    // const App = document.createElement('div');
+    // App.textContent = "basic.ex04 - Hello Wolrd";
+
+    // const App = React.createElement('div',null,'basic.ex05 - Hello World');
+    // return App;
+
+    return (
+        <div>
+            basic.ex05 - Hello World
+            <ul>
+                {
+                    ['1st','2nd'].map((e) => {
+                        return <li>${e}</li>
+                    })
+                }
+            </ul>
+        </div>
+    );
 }
 
 export{App};
