af1a4e5fdf1286223f2a172575fe24df8b3188f6
kimdongjooon <ehdwns6745@naver.com>
2023-10-31 10:53:50 +0900

    babel #1

--- 1.basics/ex05/src/App.js
+++ 1.basics/ex05/src/App.js
@@ -4,21 +4,21 @@ function App() {
     // const App = document.createElement('div');
     // App.textContent = "basic.ex04 - Hello Wolrd";
 
-    // const App = React.createElement('div',null,'basic.ex05 - Hello World');
-    // return App;
+    const App = React.createElement('div',null,'basic.ex05 - Hello World');
+    return App;
 
-    return (
-        <div>
-            basic.ex05 - Hello World
-            <ul>
-                {
-                    ['1st','2nd'].map((e) => {
-                        return <li>${e}</li>
-                    })
-                }
-            </ul>
-        </div>
-    );
+    // return (
+    //     <div>
+    //         basic.ex05 - Hello World
+    //         <ul>
+    //             {
+    //                 ['1st','2nd'].map((e) => {
+    //                     return <li>${e}</li>
+    //                 })
+    //             }
+    //         </ul>
+    //     </div>
+    // );
 }
 
 export{App};
---
+++ 9.babel-practice/ex02/readme.txt
@@ -0,0 +1,9 @@
+2. Babel Cli 
+    1) 설치
+        $ npm i -D @babel/core @babel/cli
+    2) 사용법
+        $ npx babel src -o dist
+
+    3) 변환 규칙은 플러그인(plugin) 또는 프리셋(reset) 가지고 있고 babel 설정을 통해 적용한다.
+    4) 설치
+        $ npm i -D @babel/core
